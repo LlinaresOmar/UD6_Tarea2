12fab759fd65a7101c6aebff04852bd904293bf1
Omar <llinares.omar@gmail.com>
2024-03-11 18:29:05 +0100

    7º Commit
    Rehecho clase tests

--- src/es/iesmz/tests/DiasLluviaTest.java
+++ src/es/iesmz/tests/DiasLluviaTest.java
@@ -1,6 +1,22 @@
 package es.iesmz.tests;
 
+import static org.junit.jupiter.api.Assertions.*;
 
 class DiasLluviaTest {
 
+    @org.junit.jupiter.api.Test
+    void consultaDia() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void contarDiasLluvia() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void trimestreLluvioso() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void primerDiaLluvia() {
+    }
 }
